ac4d244362b38709745191d6e898ed1e9dafb830
Rimao416 <kayumbaomari27@gmail.com>
2025-02-25 13:41:04 +0100

    Final

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,6 @@ Route::resource('articles', ArticleController::class);
 // Route::get('/', [ArticleController::class, 'index'])->name('articles.index');
 // Route::post('/articles', [ArticleController::class, 'store'])->name('articles.store');
 // Route::get('/articles/create', [ArticleController::class, 'create'])->name('articles.create');
-// Route::get('/articles/{id}', [ArticleController::class, 'show'])->name('articles.show');
 // Route::get('/articles/{id}/edit', [ArticleController::class, 'edit'])->name('articles.edit');
 // Route::delete('/articles/{id}', [ArticleController::class, 'destroy'])->name('articles.destroy');
 // Route::put('/articles/{id}', [ArticleController::class, 'update'])->name('articles.update');
